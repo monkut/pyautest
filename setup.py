a2301a49356bdec0679bf89d9508e00fe8bc5788
higumachan <harekumo792154@gmail.com>
2019-02-12 21:13:05 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="pyautest",
+	version="0.0.1",
+	description="golden file test utility",
+	url="https://github.com/higumachan/pyautest",
+	packages=find_packages(),
+)
