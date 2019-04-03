dabe28948bbd7f3c4f9342b911c20f4f38d2558b
monkut <shane.cousins@gmail.com>
2019-04-04 00:24:51 +0900

    incrementing version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="pyautest",
-	version="0.0.1",
+	version="0.0.2",
 	description="golden file test utility",
 	url="https://github.com/higumachan/pyautest",
 	packages=find_packages(),
